f3c9b8e4d036ec46b6f7978c90ff57fe7e5f1e0f
Ravendra kumar <111950764+ravendrakum@users.noreply.github.com>
2024-05-20 04:53:01 -0700

    Update index.js

--- netflix-ui/src/store/index.js
+++ netflix-ui/src/store/index.js
@@ -81,7 +81,7 @@ import {
     async (email) => {
       const {
         data: { movies },
-      } = await axios.get(`${window.location.origin}/api/user/liked/${email}`);
+      } = await axios.get(`https://net-resume-1.onrender.com/api/user/liked/${email}`);
       return movies;
     }
   );
@@ -91,7 +91,7 @@ import {
     async ({ movieId, email }) => {
       const {
         data: { movies },
-      } = await axios.put(`${window.location.origin}/api/user/remove`, {
+      } = await axios.put(`https://net-resume-1.onrender.com/api/user/remove`, {
         email,
         movieId,
       });
